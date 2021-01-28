351a50bbd4a512b2ac7e38630398c49d8d4c6e85
Hamid <ghas0012@algonquinlive.com>
2021-01-27 23:42:36 -0500

    TestPush

---
+++ ShadowStalk/Source/ShadowStalk/ShadowStalkGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ShadowStalkGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHADOWSTALK_API AShadowStalkGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
